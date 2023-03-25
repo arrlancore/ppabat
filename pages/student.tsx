f1ae7d5d4196b9d7be31d54631f596b0f6ad9a37
arrlancores <arrlancore@gmail.com>
2023-03-25 22:09:03 +0700

    add api excel

--- pages/student.tsx
+++ pages/student.tsx
@@ -1,6 +1,18 @@
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
-import { Table, ScrollArea, Menu, Divider, Drawer, Text, Button } from '@mantine/core';
+import {
+  Table,
+  ScrollArea,
+  Menu,
+  Divider,
+  Drawer,
+  Text,
+  Button,
+  Group,
+  Avatar,
+  useMantineTheme,
+  ActionIcon,
+} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { notifications } from '@mantine/notifications';
 
@@ -11,6 +23,86 @@ import EditUserForm, { Profile } from '@/features/student/components/EditUser';
 import DeleteModal from '@/features/student/components/DeleteModal';
 import DashboardLayout from '@/components/DashboardLayout';
 // import SendMessageForm from '@/features/student/components/SendMessage';
+
+import {
+  IconLogout,
+  IconHeart,
+  IconStar,
+  IconMessage,
+  IconSettings,
+  IconPlayerPause,
+  IconTrash,
+  IconSwitchHorizontal,
+  IconChevronRight,
+  IconDots,
+} from '@tabler/icons-react';
+
+export function UserMenu() {
+  const theme = useMantineTheme();
+  return (
+    <Group position="center">
+      <Menu
+        withArrow
+        width={300}
+        position="bottom"
+        transitionProps={{ transition: 'pop' }}
+        withinPortal
+      >
+        <Menu.Target>
+          <ActionIcon>
+            <IconDots size="1rem" stroke={1.5} />
+          </ActionIcon>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item rightSection={<IconChevronRight size="0.9rem" stroke={1.5} />}>
+            <Group>
+              <Avatar
+                radius="xl"
+                src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
+              />
+
+              <div>
+                <Text weight={500}>Nancy Eggshacker</Text>
+                <Text size="xs" color="dimmed">
+                  neggshaker@mantine.dev
+                </Text>
+              </div>
+            </Group>
+          </Menu.Item>
+
+          <Menu.Divider />
+
+          <Menu.Item icon={<IconHeart size="0.9rem" stroke={1.5} color={theme.colors.red[6]} />}>
+            Liked posts
+          </Menu.Item>
+          <Menu.Item icon={<IconStar size="0.9rem" stroke={1.5} color={theme.colors.yellow[6]} />}>
+            Saved posts
+          </Menu.Item>
+          <Menu.Item icon={<IconMessage size="0.9rem" stroke={1.5} color={theme.colors.blue[6]} />}>
+            Your comments
+          </Menu.Item>
+
+          <Menu.Label>Settings</Menu.Label>
+          <Menu.Item icon={<IconSettings size="0.9rem" stroke={1.5} />}>Account settings</Menu.Item>
+          <Menu.Item icon={<IconSwitchHorizontal size="0.9rem" stroke={1.5} />}>
+            Change account
+          </Menu.Item>
+          <Menu.Item icon={<IconLogout size="0.9rem" stroke={1.5} />}>Logout</Menu.Item>
+
+          <Menu.Divider />
+
+          <Menu.Label>Danger zone</Menu.Label>
+          <Menu.Item icon={<IconPlayerPause size="0.9rem" stroke={1.5} />}>
+            Pause subscription
+          </Menu.Item>
+          <Menu.Item color="red" icon={<IconTrash size="0.9rem" stroke={1.5} />}>
+            Delete account
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    </Group>
+  );
+}
 
 const MOCKUP_USERS = [
   {
@@ -144,7 +236,8 @@ export default function Users(/*props*/) {
           <td>{user.workplace}</td>
           <td>{user.phone}</td>
           <td>
-            <Menu>
+            <UserMenu />
+            {/* <Menu>
               <Menu.Target>
                 <Button variant="subtle">
                   <MoreHorizontal />
@@ -172,7 +265,7 @@ export default function Users(/*props*/) {
                   Hapus
                 </Menu.Item>
               </Menu.Dropdown>
-            </Menu>
+            </Menu> */}
           </td>
         </tr>
       ))
